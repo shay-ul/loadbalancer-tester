18813963f6cebf6201e566483e93ae612ecbe68e
Shay Ulmer <shayulmer@gmail.com>
2023-01-10 16:53:02 +0200

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import socket
+print(())
+
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return ("<html><head><title>Loadbalancer Tester</title></head><body><h1> Hello from " + socket.gethostname()) + "</h1></body></html>"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=80)
